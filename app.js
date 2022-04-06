052c3d5fb45872ff2ea7b92c8c4573b517515a74
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-06 19:32:00 +0530

    refactor the code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var btnTranslate=document.querySelector('.btn');
-var txtInput=document.querySelector('.txt-input');
-var output=document.querySelector('.txt-output');
-var serverURL='https://api.funtranslations.com/translate/chef.json';
+var btnTranslate = document.querySelector('.btn');
+var txtInput = document.querySelector('.txt-input');
+var output = document.querySelector('.txt-output');
+var serverURL = 'https://api.funtranslations.com/translate/chef.json';
 // Speech Recognition
-var speechRecognition=speechRecognition|| webkitSpeechRecognition; 
-var recognition=new speechRecognition();
+var speechRecognition = speechRecognition || webkitSpeechRecognition;
+var recognition = new speechRecognition();
 // Speech Synthesizer
-function textToAudio(message){
+function textToAudio(message) {
     let speech = new SpeechSynthesisUtterance();
     speech.lang = "en-US";
     speech.text = message;
@@ -15,7 +15,9 @@ function textToAudio(message){
     speech.pitch = 1;
     window.speechSynthesis.speak(speech);
 }
+// Call Event Listener on clicking btn
 btnTranslate.addEventListener("click", clickHandler);
+
 function clickHandler() {
     recognition.start();
     console.log('start speech');
@@ -28,24 +30,26 @@ function clickHandler() {
         txtInput.innerText = transcriptInput;
         console.log(event.results[0][0]);
         console.log(txtInput.value)
-function getTranslation(Txt) {
-    return serverURL + "?text=" + Txt;
-}
-function errorHandler(error){
-    alert(`Exceeded limitations!! Try again later`);
-}
 
-var translatedText = () => {
-    var Input = transcriptInput;
-    fetch(getTranslation(Input))
-        .then(response => response.json())
-        .then(json => {
-            let outputText=json.contents.translated;
-            outputDiv.innerText = outputText;
-            textToAudio(outputText);
-        })
-        .catch(errorHandler)
-}
-translatedText();
-}
+        function getTranslation(Txt) {
+            return serverURL + "?text=" + Txt;
+        }
+
+        function errorHandler(error) {
+            alert(`Exceeded limitations!! Try again later`);
+        }
+
+        var translatedText = () => {
+            var Input = transcriptInput;
+            fetch(getTranslation(Input))
+                .then(response => response.json())
+                .then(json => {
+                    let outputText = json.contents.translated;
+                    outputDiv.innerText = outputText;
+                    textToAudio(outputText);
+                })
+                .catch(errorHandler)
+        }
+        translatedText();
+    }
 }
